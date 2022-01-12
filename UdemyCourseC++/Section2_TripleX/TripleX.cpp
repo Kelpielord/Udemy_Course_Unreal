45fcfa1f7b2e033842fecaa501d481221678803e
Kelpielord <nympheraa@gmail.com>
2022-01-12 20:09:08 +0300

    First commit

---
+++ UdemyCourseC++/Section2_TripleX/TripleX.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "A dryad has been waylaid by playful faeries and it has to find it's way out of the forest, but the faeries put up magical barriers!";
+    std::cout << "\nYou need to enter the correct numbers to break the magical barrier and move forward...";
+    return 0;
+}

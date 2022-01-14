b3621c6357e564c24ce0e2708dce6f90b94316a4
Kelpielord <nympheraa@gmail.com>
2022-01-14 11:35:19 +0300

    TripleX added user input

--- UdemyCourseC++/Section2_TripleX/TripleX.cpp
+++ UdemyCourseC++/Section2_TripleX/TripleX.cpp
@@ -2,7 +2,36 @@
 
 int main() 
 {
+    //Print welcome message to the terminal
     std::cout << "A dryad has been waylaid by playful faeries and it has to find it's way out of the forest, but the faeries put up magical barriers!";
-    std::cout << "\nYou need to enter the correct numbers to break the magical barrier and move forward...";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct numbers to break the magical barrier and move forward..." << std::endl;
+
+    const int CodeNumA = 2;
+    const int CodeNumB = 3;
+    const int CodeNumC = 4;
+
+    const int CodeSum = CodeNumA + CodeNumB + CodeNumC;
+    const int CodeProduct = CodeNumA * CodeNumB * CodeNumC;
+
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers on the barrier" << std::endl;
+    std::cout << "+ The numbers add up to: " << CodeSum << std::endl;
+    std::cout << "+ The numbers multiply to give: " << CodeProduct << std::endl;
+
+    int n;
+    int GuessOne, GuessTwo, GuessThree;
+    std::cin >> n;
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cin >> GuessOne;
+        std::cin >> GuessTwo;
+        std::cin >> GuessThree;
+
+        std::cout << "You entered: " << GuessOne << GuessTwo << GuessThree;
+    }
+    
+  
+
     return 0;
 }

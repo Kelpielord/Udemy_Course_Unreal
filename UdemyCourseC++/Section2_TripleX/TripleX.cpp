efa4463807755c6c8aa5f3fdb0462f56f390bc5e
Kelpielord <nympheraa@gmail.com>
2022-01-26 16:38:41 +0300

    Optimized code and made progress on TripleX

--- UdemyCourseC++/Section2_TripleX/TripleX.cpp
+++ UdemyCourseC++/Section2_TripleX/TripleX.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
-int main() 
+void PrintIntro(int Difficulty) 
 {
     //Print welcome message to the terminal
-    std::cout << "A dryad has been waylaid by playful faeries and it has to find it's way out of the forest, but the faeries put up magical barriers!";
-    std::cout << std::endl;
-    std::cout << "You need to enter the correct numbers to break the magical barrier and move forward..." << std::endl;
+    std::cout << "\n\nA dryad has been waylaid by playful faeries and it has to find it's way out of the level " << Difficulty << " forest, but the faeries put up magical barriers!\n";
+    std::cout << "You need to enter the correct numbers to break the magical barrier and move forward...\n\n";
+}
 
+bool PlayGame(int Difficulty)
+{
+    PrintIntro(Difficulty);
     const int CodeNumA = 2;
     const int CodeNumB = 3;
     const int CodeNumC = 4;
@@ -15,28 +18,46 @@ int main()
     const int CodeProduct = CodeNumA * CodeNumB * CodeNumC;
 
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers on the barrier" << std::endl;
-    std::cout << "+ The numbers add up to: " << CodeSum << std::endl;
-    std::cout << "+ The numbers multiply to give: " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers on the barrier\n";
+    std::cout << "\n+ The numbers add up to: " << CodeSum;
+    std::cout << "\n+ The numbers multiply to give: " << CodeProduct << std::endl;
 
-    int n;
+    //Store player guess
     int GuessOne, GuessTwo, GuessThree;
+    std::cin >> GuessOne >> GuessTwo >> GuessThree;
 
-    std::cin >> GuessOne;
-    std::cin >> GuessTwo;
-    std::cin >> GuessThree;
-    
     int GuessSum = GuessOne + GuessTwo + GuessThree;
     int GuessProduct = GuessOne * GuessTwo * GuessThree;
 
+    //Check if player input is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
     {
-        std::cout << "You win!";
+        std::cout << "You managed to destroy the barrier!";
+        return true;
     }
     else 
     {
-        std::cout << "You lose!";
+        std::cout << "You didn't destroy the barrier...";
+        return false;
     }
+}
 
+int main() 
+{
+    int LevelDifficulty = 1;
+    const int maxDifficulty = 5;
+    while (LevelDifficulty <= maxDifficulty)
+    {
+        bool BLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+
+        if (BLevelComplete)
+        {
+            LevelDifficulty++;
+        }
+        
+    }
+    
     return 0;
 }
